5c53bc81dade107254157e729b365a3d78f6cd96
jankleinert <jkleiner@redhat.com>
2019-08-26 13:52:13 -0400

    Updated to hosted-mode

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,6 +1,7 @@
 WORKSHOP_NAME=lab-getting-started
 WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-getting-started:ocp-4.2
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.2
+SPAWNER_MODE=hosted-workshop
 SPAWNER_PASSWORD=openshift
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=7200
